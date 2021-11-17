b934615cfc17bbf72b1eb3e126ca83aa912e2ab4
AhrenHart <b00345114@studentmail.uws.ac.uk>
2021-11-17 15:39:28 +0000

    update - Parallax scrolling added

--- PhaserConfig.js
+++ PhaserConfig.js
@@ -3,8 +3,8 @@ var config = {
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600,
+        width: 384,
+        height: 240,
     },
     physics: {
         default: 'arcade',
