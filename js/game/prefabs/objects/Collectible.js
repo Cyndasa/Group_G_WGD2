e2d3031dea16be0e847b7c11f7606b48cdb3b2b3
Cyndasa <adbauld@googlemail.com>
2021-11-18 23:28:44 +0000

    Added a Collectible prefab class

---
+++ js/game/prefabs/objects/Collectible.js
@@ -0,0 +1,26 @@
+class Collectible extends Phaser.Physics.Arcade.Sprite{
+
+/* Collectible Prefab */
+
+    /* Declare Variables */
+
+    /* Constructor for placing object in scene */
+    constructor(scene, x, y,) {
+        super(scene, x, y, "collectible");
+
+        scene.add.existing(this);
+        scene.physics.world.enableBody(this);
+        this.body.setCollideWorldBounds(true);
+
+    }
+
+    create(){
+        console.log('The Collectible Exists'); // Debug Line, comment out or delete
+
+    }
+
+    update(){
+
+    }
+
+}
