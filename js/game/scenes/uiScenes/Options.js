fa07194b76766770214f9fcefa36de75d180fa29
AhrenHart <b00345114@studentmail.uws.ac.uk>
2021-12-07 02:48:28 +0000

    updates

--- js/game/scenes/uiScenes/Options.js
+++ js/game/scenes/uiScenes/Options.js
@@ -8,9 +8,12 @@ class optionsMenu extends Phaser.Scene {
     };
 
     preload(){
-/*        this.load.video('video', '../videos/MenuVideo.webm');
+        this.load.video('video', '../videos/MenuVideo.webm');
         this.load.image('exit', '../../../../gameAssets/imageAssets/uiImages/Buttons/Exit/Exit-Unclicked.png');
-        this.load.image('title', '../../../../gameAssets/imageAssets/uiImages/Title/Title.png');*/
+        this.load.image('title', '../../../../gameAssets/imageAssets/uiImages/Title/Title.png');
+        this.load.image('muteButton', '../../../../gameAssets/imageAssets/uiImages/Buttons/MuteButtons/Mute.png')
+        this.load.image('controls', '../../../../gameAssets/imageAssets/uiImages/Buttons/Controls/Controls.png')
+       /* this.load.image('muteButton', '../../../../gameAssets/imageAssets/uiImages/Buttons/MuteButtons/ButtonsSpriteSheet.png');*/
     }
 
     create(){
@@ -23,27 +26,45 @@ class optionsMenu extends Phaser.Scene {
         this.add.image (400, 100 , 'title').setScale(3);
 
         /* Mute all sounds button */
-/*        const muteButton = this.add.image(400, 300, '');
+
+       const muteButton = this.add.image(400, 230, 'muteButton');
         muteButton.setInteractive();
         muteButton.setScale(1.5);
-        muteButton.on('pointerover', ()=>{
+        muteButton.on('pointerup', ()=>{
             if(!this.sound.mute){
                 this.sound.mute = true;
+
             }
             else{
                 this.sound.mute = false;
-            }
-        });*/
 
+            }
+        });
         /* Apply tint when hovered over */
-/*        muteButton.on('pointerover', ()=>{
-            mute.setTint('');
-        });*/
+       muteButton.on('pointerup', ()=>{
+            muteButton.setTint('0xFF00F5');
+        });
 
         /* Remove tint when no longer hovering over */
-/*        muteButton.on('pointerout', ()=>{
-            mute.clearTint();
-        });*/
+      muteButton.on('pointerout', ()=>{
+            muteButton.clearTint();
+        });
+
+        const ControlButton = this.add.image(400, 330, 'controls');
+        ControlButton .setInteractive();
+        ControlButton .setScale(1.5);
+        ControlButton .on('pointerup', ()=>{
+            this.scene.start('Menu');
+        });
+        /* Apply tint when hovered over */
+        ControlButton .on('pointerover', ()=>{
+            ControlButton .setTint('0xFF00F5');
+        });
+
+        /* Remove tint when no longer hovering over */
+        ControlButton .on('pointerout', ()=>{
+            ControlButton .clearTint();
+        });
 
 
         var ExitButton = this.add.image(400, 430, 'exit').setScale(1.5);
